ca5b6ab5a9045ca91cd1450e44aeeb9de980523c
unknown <sanchit2005yudavanshi@gmail.com>
2023-03-14 21:56:47 +0530

    bot has been logged in successfully

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 require('dotenv').config(); // configured dotenv to set the token of our discord bot .
-const Discord = require("discord.js"); 
-
-// creating the client to log our bot in the server .
-const client = new Discord.Client();
+const { Client, GatewayIntentBits } = require('discord.js');
+// creating new client.
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready',()=>{
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// make sure this line comes in the last to login the user.
 
+client.login(process.env.CLIENT_TOKEN); // loggin bot in the server
